1158897d33f9b42068832a9e09d4bacda37af629
Flavio Milan <oflaviomilan@gmail.com>
2023-07-27 20:04:07 -0300

    imp: pluralsight 101 - 2

---
+++ cpp/pluralsight/1-learn-to-program-with-cpp-17/1.2-streamio-and-local-variables/1.2.2-local-variables/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+  float rate = 0.23;
+  int limit = 100;
+}
---
+++ cpp/pluralsight/1-learn-to-program-with-cpp-17/1.2-streamio-and-local-variables/1.2.3-type-safety/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main() {
+
+  std::cout << 42 << '\n';
+  std::cout << "Hello" << '\n';
+
+  std::cout << '\n' << '\n';
+  int i = 2 + 2;
+  std::cout << '\n' << '\n';
+  i = i * 3;
+  std::cout << i << '\n';
+}
